7924a3e7db2f4e559d39438185511fe61065c734
Dleon285 <dleonbriones@gmail.com>
2023-09-28 22:21:08 -0500

    calificaciones

--- src/Controller/UtilitarioController.php
+++ src/Controller/UtilitarioController.php
@@ -6,8 +6,16 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Process\Process;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 class UtilitarioController extends AbstractController
 {
+    private $mailer;
+
+    public function __construct(MailerInterface $mailer)
+    {
+        $this->mailer = $mailer;
+    }
     /**
      * Documentación para la función 'enviaCorreo'
      * 
@@ -27,19 +35,25 @@ class UtilitarioController extends AbstractController
         $strMensajeCorreo = $arrayParametros['strMensajeCorreo'] ? $arrayParametros['strMensajeCorreo']:'';
         $strRemitente     = $arrayParametros['strRemitente'] ? $arrayParametros['strRemitente']:'';
         $strDestinatario  = $arrayParametros['strDestinatario'] ? $arrayParametros['strDestinatario']:'';
-        $strRutaImagen    = $arrayParametros['strRutaImagen']   ? $arrayParametros['strRutaImagen']:'';
-        $strRespuesta     = 'Ok';
+        $strRespuesta     = 'Ok'; error_log($strAsunto); error_log($strMensajeCorreo); error_log($strRemitente); error_log($strDestinatario);
         try
         {
             error_log("1.2");
-            $objMessage =  (new \Swift_Message())->setSubject($strAsunto)
+            /*$objMessage =  (new \Swift_Message())->setSubject($strAsunto)
                                                  ->setFrom($strRemitente)
                                                  ->setTo($strDestinatario)
-                                                 ->setBody($strMensajeCorreo, 'text/html');
+                                                 ->setBody($strMensajeCorreo, 'text/html');*/
+
+            $email = (new Email())
+                                ->from($strRemitente)
+                                ->to($strDestinatario)
+                                ->subject($strAsunto)
+                                ->text($strMensajeCorreo);
                                                  error_log("1.3");
-            $strRespuesta = $this->get('mailer')->send($objMessage);
+            $strRespuesta = $this->mailer->send($email);
             error_log("1.4");
         } catch (\Exception $e) {
+            error_log("error111111111111111111111111111111");error_log($e);
            return  $e->getMessage();
         }
         return $strRespuesta;
